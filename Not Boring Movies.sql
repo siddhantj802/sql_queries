e5064e871213ef030d56aaca195f42601087c962
Siddhant <65533728+siddhantj802@users.noreply.github.com>
2023-07-11 08:02:25 +0530

    Create Not Boring Movies.sql

---
+++ Not Boring Movies.sql
@@ -0,0 +1,11 @@
+select x.odd_ids as id , x.movie,x.description,x.rating
+from
+(select movie, description, rating,
+case 
+    when id%2 <> 0 then id
+    end as odd_ids
+from cinema
+where description <> 'boring' 
+)x
+where x.odd_ids is not null
+order by x.rating desc
