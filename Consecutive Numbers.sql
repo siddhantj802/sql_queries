ed78de4df839b006cd3b8e2a89411dcf09cdc729
Siddhant <65533728+siddhantj802@users.noreply.github.com>
2023-07-22 17:05:00 +0530

    Create Consecutive Numbers.sql

---
+++ Consecutive Numbers.sql
@@ -0,0 +1,8 @@
+select distinct(num) as consecutiveNums
+from
+(select * , 
+lead(num ,1) over(order by id) as nxt ,
+lead(num , 2) over(order by id) as nxttonxt
+from Logs 
+order by id ) x
+where x.num = x.nxt and x.num = x.nxttonxt
