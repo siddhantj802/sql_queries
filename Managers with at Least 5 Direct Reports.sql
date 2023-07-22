904ba7c33d0f503e1dcec26f864f1069440d5df4
Siddhant <65533728+siddhantj802@users.noreply.github.com>
2023-07-22 18:49:35 +0530

    Update Managers with at Least 5 Direct Reports.sql
    +-------------+---------+
    | Column Name | Type    |
    +-------------+---------+
    | id          | int     |
    | name        | varchar |
    | department  | varchar |
    | managerId   | int     |
    +-------------+---------+
    In SQL, id is the primary key column for this table.
    Each row of this table indicates the name of an employee, their department, and the id of their manager.
    If managerId is null, then the employee does not have a manager.
    No employee will be the manager of themself.
     
    
    Find the managers with at least five direct reports.
    
    Return the result table in any order.
    
    The result format is in the following example.
    
     
    
    Example 1:
    
    Input: 
    Employee table:
    +-----+-------+------------+-----------+
    | id  | name  | department | managerId |
    +-----+-------+------------+-----------+
    | 101 | John  | A          | None      |
    | 102 | Dan   | A          | 101       |
    | 103 | James | A          | 101       |
    | 104 | Amy   | A          | 101       |
    | 105 | Anne  | A          | 101       |
    | 106 | Ron   | B          | 101       |
    +-----+-------+------------+-----------+
    Output: 
    +------+
    | name |
    +------+
    | John |
    +------+

--- Managers with at Least 5 Direct Reports.sql
+++ Managers with at Least 5 Direct Reports.sql
@@ -1,5 +1,4 @@
 select distinct(x.name)
-
 from 
 ( select emp1.id , emp1.name , emp2.managerId,
 row_number() over(partition by managerId order by id) as rn
@@ -7,5 +6,6 @@ from employee emp1
 join employee emp2 
 on emp1.id = emp2.managerId
 ) x 
-
 where x.rn >= 5
+
+# see the extended comment
